e7ed4378f577fef4cb560cf2fe42433506572c53
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-23 19:17:01 +0530

    def function

---
+++ Python/day 1/python.py
@@ -0,0 +1,8 @@
+# i have already done some data types now just create a file 
+# def greet():
+#     print("hello robot")
+# greet()
+
+def robot_status(name, battery):
+    print(f"robot {name} is ready ! \n Alita health is {battery} %")
+robot_status("Alita", 85)
