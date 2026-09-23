fe8a5330f861c7f454eb3bb20bbb8a21304d6ad9
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-23 20:13:06 +0530

    use for and while loop

--- Python/day 1/python.py
+++ Python/day 1/python.py
@@ -3,6 +3,65 @@
 #     print("hello robot")
 # greet()
 
-def robot_status(name, battery):
-    print(f"robot {name} is ready ! \n Alita health is {battery} %")
-robot_status("Alita", 85)
+# def robot_status(name, battery):
+#     print(f"robot {name} is ready ! \n Alita health is {battery} %")
+# robot_status("Alita", 85)
+
+# def robot_status(name, battery, temprature):
+#     print(f"robot {name} is ready !")
+#     if battery <= 20:
+#         print("WARNING ! LOW BATTERY !")
+#     else:
+#         print(f"Alita health is ok ")
+
+#     if temprature > 40:
+#         print("WARNING ! HIGH TEMPERATURE !")
+#     else:
+#         print(f"Alita temprature is ok ")
+# robot_status("Alita", 15, 41)
+
+# import time
+# for i in range(5):
+#     print("checking ...!")
+#     time.sleep(1)  
+# time.sleep(1)
+# print("Alita is ready 😉")
+
+# import time
+# print("checking ...!")
+# time.sleep(1)
+# print("Alita is ready")
+
+# import time
+# count = 1
+# while count <= 5:
+#     print(f" robot checking ...! {count}")
+#     time.sleep(1)
+#     count = count + 1
+
+# import time
+# sensor_active = True
+# while sensor_active:
+#     print("robot is scanning ...!")
+#     time.sleep(1)
+
+#     sensor_active = False
+# print("scanning stop")
+import time
+# count = 1
+# while count <= 10:
+#     print(f"robot checking ...! {count}")
+#     time.sleep(1)
+
+#     if count == 5:
+#         print("Emergency Stop ⚠️")
+#         break
+#     count = count + 1
+
+for i in range(1, 10):
+    print(f"robot checking ...! {i}")
+    time.sleep(1)
+
+    if i == 5:
+        print("Emergency Stop ⚠️")
+        break
